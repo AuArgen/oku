6276fa3e362a64a7d08de9b3cbb3765cba4ff9f7
AuArgen <argenazizbekuulu3@gmail.com>
2023-11-09 11:35:40 +0600

    ...

---
+++ index.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</head>
+<body class=" bg-light">
+  <div class="container">
+    <div class="row">
+      <!-- blog start -->
+      <div class="col-lg-3 col-md-4 col-sm-6 col-12 p-2">
+        <div class="w-100 row bg-white rounded shadow">
+          <div class="col-6">
+            <h2 class="text-center">
+              65%
+            </h2>
+            <p>
+              Lorem ipsum dolor sit,
+            </p>
+          </div>
+          <div class="col-4">
+            <img class="w-100" src="https://illustoon.com/photo/3311.png" alt="" srcset="">
+          </div>
+        </div>
+      </div>
+      <!-- blog end -->
+      <!-- blog start -->
+      <div class="col-lg-3 col-md-4 col-sm-6 col-12 p-2">
+        <div class=" w-100 row bg-white rounded shadow">
+          <div class="col-4">
+            <img class="w-100" src="https://illustoon.com/photo/3311.png" alt="" srcset="">
+          </div>
+          <div class="col-6">
+            <h2 class="text-center">
+              65%
+            </h2>
+            <p>
+              Lorem ipsum dolor sit,
+            </p>
+          </div>
+        </div>
+      </div>
+      <!-- blog end -->
+      <!-- blog start -->
+      <div class="col-lg-3 col-md-4 col-sm-6 col-12 p-2">
+        <div class=" w-100 row bg-white rounded shadow">
+          <div class="col-4">
+            <img class="w-100" src="https://illustoon.com/photo/3311.png" alt="" srcset="">
+          </div>
+          <div class="col-6">
+            <h2 class="text-center">
+              65%
+            </h2>
+            <p>
+              Lorem ipsum dolor sit,
+            </p>
+          </div>
+        </div>
+      </div>
+      <!-- blog end -->
+      <!-- blog start -->
+      <div class="col-lg-3 col-md-4 col-sm-6 col-12 p-2">
+        <div class=" w-100 row bg-white rounded shadow">
+          <div class="col-4">
+            <img class="w-100" src="https://illustoon.com/photo/3311.png" alt="" srcset="">
+          </div>
+          <div class="col-6">
+            <h2 class="text-center">
+              65%
+            </h2>
+            <p>
+              Lorem ipsum dolor sit,
+            </p>
+          </div>
+        </div>
+      </div>
+      <!-- blog end -->
+      <!-- blog start -->
+      <div class="col-lg-3 col-md-4 col-sm-6 col-12 p-2">
+        <div class=" w-100 row bg-white rounded shadow">
+          <div class="col-4">
+            <img class="w-100" src="https://illustoon.com/photo/3311.png" alt="" srcset="">
+          </div>
+          <div class="col-6">
+            <h2 class="text-center">
+              65%
+            </h2>
+            <p>
+              Lorem ipsum dolor sit,
+            </p>
+          </div>
+        </div>
+      </div>
+      <!-- blog end -->
+      <!-- blog start -->
+      <div class="col-lg-3 col-md-4 col-sm-6 col-12 p-2">
+        <div class=" w-100 row bg-white rounded shadow">
+          <div class="col-4">
+            <img class="w-100" src="https://illustoon.com/photo/3311.png" alt="" srcset="">
+          </div>
+          <div class="col-6">
+            <h2 class="text-center">
+              65%
+            </h2>
+            <p>
+              Lorem ipsum dolor sit,
+            </p>
+          </div>
+        </div>
+      </div>
+      <!-- blog end -->
+      <!-- blog start -->
+      <div class="col-lg-3 col-md-4 col-sm-6 col-12 p-2">
+        <div class=" w-100 row bg-white rounded shadow">
+          <div class="col-4">
+            <img class="w-100" src="https://illustoon.com/photo/3311.png" alt="" srcset="">
+          </div>
+          <div class="col-6">
+            <h2 class="text-center">
+              65%
+            </h2>
+            <p>
+              Lorem ipsum dolor sit,
+            </p>
+          </div>
+        </div>
+      </div>
+      <!-- blog end -->
+      <!-- blog start -->
+      <div class="col-lg-3 col-md-4 col-sm-6 col-12 p-2">
+        <div class=" w-100 row bg-white rounded shadow">
+          <div class="col-4">
+            <img class="w-100" src="https://illustoon.com/photo/3311.png" alt="" srcset="">
+          </div>
+          <div class="col-6">
+            <h2 class="text-center">
+              65%
+            </h2>
+            <p>
+              Lorem ipsum dolor sit,
+            </p>
+          </div>
+        </div>
+      </div>
+      <!-- blog end -->
+      <!-- blog start -->
+      <div class="col-lg-3 col-md-4 col-sm-6 col-12 p-2">
+        <div class=" w-100 row bg-white rounded shadow">
+          <div class="col-4">
+            <img class="w-100" src="https://illustoon.com/photo/3311.png" alt="" srcset="">
+          </div>
+          <div class="col-6">
+            <h2 class="text-center">
+              65%
+            </h2>
+            <p>
+              Lorem ipsum dolor sit,
+            </p>
+          </div>
+        </div>
+      </div>
+      <!-- blog end -->
+      
+    </div>
+  </div>
+</body>
+</html>
